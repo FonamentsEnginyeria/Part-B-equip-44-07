8d93510523b38400b790179c526d7642a94e8283
Judit <judit.lopezj@e-campus.uab.cat>
2020-12-02 23:55:19 +0100

    oscars.sh té el menu

--- oscars.sh
+++ oscars.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+echo "Base de Dades d'actors i actrius guanyador/es de l'Oscar"
+echo "1. Històric d'Oscars."
+echo "2. Qui va guanyar...?"
+echo "3. Ets un expert?"
+echo "4. Altes, baixes i modificacions."
+echo "0. Sortir."
+
+read var 
+while [$var -ne 0]
+do
+read 
+	case 
+	
+	
+	esac
+
+done
+
