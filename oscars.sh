14ea602aaa3fc9527e842e433d544c6982b34984
Judit <judit.lopezj@e-campus.uab.cat>
2020-12-03 11:35:34 +0100

    El menu està acabat

--- oscars.sh
+++ oscars.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sortir=0
+
+while [ $sortir -ne 1 ]
+do
+
 echo "Base de Dades d'actors i actrius guanyador/es de l'Oscar"
 echo "1. Històric d'Oscars."
 echo "2. Qui va guanyar...?"
@@ -7,14 +12,24 @@ echo "3. Ets un expert?"
 echo "4. Altes, baixes i modificacions."
 echo "0. Sortir."
 
-read var 
-while [$var -ne 0]
-do
-read 
-	case 
-	
-	
-	esac
+read opcio 
+case $opcio in
+0)sortir=1;;
+1) echo "En desenvolupament"
+read -p "$*";;
+2) echo "En desenvolupament"
+read -p "$*";;
+3) echo "En desenvolupament"
+read -p "$*";;
+4) echo "En desenvolupament"
+read -p "$*";;
+*) echo "Error, caràcter $opcio no vàlid"
+read -p "$*";;
+esac
+
+clear
+
+done
 
 done
 
