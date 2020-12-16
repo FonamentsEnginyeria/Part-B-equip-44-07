d701a24da49bfc15fcc7bf099dad0a475300ed01
adria.martinezmu@e-campus.uab.cat <adria.martinezmu@e-campus.uab.cat>
2020-12-16 17:42:10 +0100

    Branca finalitzada sense problemes

---
+++ Opcio1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+echo "Històric d’Oscars"
+echo "------------------"
+echo "1.1 Mostrar històric d’Oscars a millor actor (alfabètic). "
+echo "1.2 Mostrar històric d’Oscars a millor actriu (edat). "
+echo "1.3 Mostrar històric d’Oscars (actors/actrius). "
+
+read i 
+
+
+case $i in 
+
+	1)	
+	echo "Edat	Actor		Any	Pelicula"
+	tail +2 oscar_age_male.csv | sort -k 4 >> aux
+	awk -F "\"*,\"*" '{print $4, $2, $3, $5}' aux
+	rm aux
+	
+	2) 	
+	echo "Actriu		Any	Edat	Pelicula"
+	tail +2 oscar_age_female.csv | sort -k 3 >> aux
+	awk -F "\"*,\"*" '{print $3, $4, $2, $5}' aux
+	rm aux
+	
+	3)	echo "Any	Actor/Actriu		Edat	Pelicula"
+	tail +2 oscar_age_female.csv | sort -k 2 >> aux
+	tail +2 oscar_age_female.csv | sort -k 2 >> aux
+	sort -k 2 aux
+	awk -F "\"*,\"*" '{print $2, $4, $3, $5}' aux
+	rm aux
+	
+	*)
+	
+esac
+
